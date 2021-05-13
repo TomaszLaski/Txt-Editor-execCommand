a018689e04d6426e3be9b289d21656883a3690fe
Tomasz Łaski <tomasz.laski0001@gmail.com>
2021-05-13 22:25:12 +0200

    create textfield with styles and functionality

---
+++ public/main.js
@@ -0,0 +1,9 @@
+function textBold() {
+	document.execCommand("bold", false, null);
+}
+function textItalic() {
+	document.execCommand("italic", false, null);
+}
+function textPointer() {
+	document.execCommand("insertUnorderedList", false, null);
+}
